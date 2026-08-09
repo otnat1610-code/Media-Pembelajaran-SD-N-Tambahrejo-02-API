00cc9ebe2e68a656d3656a6adeacd7dae2f0d819
otnat1610-code <otnat1610@gmail.com>
2026-08-10 06:36:31 +0700

    Update DetailKuis.php

--- app/Models/DetailKuis.php
+++ app/Models/DetailKuis.php
@@ -4,27 +4,65 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class DetailKuis extends Model
 {
     protected $table = 'tb_detail_kuis';
 
     protected $primaryKey = 'id_detail_kuis';
 
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $fillable = [
+
+        // =========================
+        // RELASI
+        // =========================
         'id_kuis',
+
+        // =========================
+        // SOAL
+        // =========================
         'pertanyaan',
+        'gambar_pertanyaan',
+
+        // =========================
+        // PILIHAN A
+        // =========================
         'pilihan_a',
+        'gambar_pilihan_a',
+
+        // =========================
+        // PILIHAN B
+        // =========================
         'pilihan_b',
+        'gambar_pilihan_b',
+
+        // =========================
+        // PILIHAN C
+        // =========================
         'pilihan_c',
+        'gambar_pilihan_c',
+
+        // =========================
+        // PILIHAN D
+        // =========================
         'pilihan_d',
+        'gambar_pilihan_d',
+
+        // =========================
+        // JAWABAN
+        // =========================
         'jawaban',
+
+        // =========================
+        // POIN
+        // =========================
         'poin',
     ];
 
-    // RELASI KUIS
+    // =========================
+    // RELASI KE KUIS
+    // =========================
     public function kuis()
     {
         return $this->belongsTo(
